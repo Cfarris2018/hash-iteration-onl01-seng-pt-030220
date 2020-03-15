c3fa66edb84bcd813e1aa600f99c2f8e85b05991
Camisha  Farris <cfarris@anointedflooring.com>
2020-03-15 04:50:17 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,11 +5,18 @@
 # }
 
 def happy_birthday(birthday_kids)
-  def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
 
-
+def age_appropriate_birthday(birthdays)
+  birthdays.each do |name, age|
+    if age <= 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
+  end
+end 
 
